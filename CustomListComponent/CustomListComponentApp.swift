afff9cbd3b0faba491b32a875ad567ac55337bfc
Ahmed Nagy <ahmednaagy97@gmail.com>
2023-12-24 23:56:10 +0200

    Initial Commit

---
+++ CustomListComponent/CustomListComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomListComponentApp.swift
+//  CustomListComponent
+//
+//  Created by Ahmed Nagy on 24/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomListComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
